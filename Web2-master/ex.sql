b52c493fde59e7f30bce2820eacb0e89c2426dce
Gabriel Vieira <gvieirabriel@gmail.com>
2018-04-05 09:24:17 -0300

    Update ex.sql

--- Web2-master/ex.sql
+++ Web2-master/ex.sql
@@ -9,6 +9,21 @@ create table tb_usuario
     login_usuario varchar(50)
 );
 
+
+create table tb_cliente
+(
+	id_cliente integer primary key auto_increment,
+	cpf_cliente char(11) ,
+	nome_cliente varchar(100),
+    	email_cliente varchar(100),
+	cep_cliente char(8),
+	rua_cliente varchar(100),
+	nr_cliente varchar(100),
+	cidade_cliente varchar(100),
+	data_cliente date,
+	uf_cliente char(2),
+);
+
 insert into tb_usuario(nome_usuario,senha_usuario,login_usuario) values ('Gabriel','1234','vrag');
 insert into tb_usuario(nome_usuario,senha_usuario,login_usuario) values ('Daniel','123456','danyhero');
 insert into tb_usuario(nome_usuario,senha_usuario,login_usuario) values ('Thiago','12341234','catchoro');
